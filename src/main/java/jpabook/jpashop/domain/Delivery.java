719b406d1bf6e224f83579a1a3ba89c0b8c0edce
beomjun <bzzun0609@github.com>
2024-01-13 23:59:23 +0900

    feat: Delivery, Order, OrderItem 엔티티 생성
    Delivery, Order: 이넘클래스 사용하므로 Enumerated-STRING 설정필수.

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    // 이넘클래스 사용 시 어노테이션 반드시 사용해야 함. EnumType은 ORDINAL(1,2,3..숫자형식임), STRING 두 가지 있음. 무조건 STRING.
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //READY, COMP
+}
