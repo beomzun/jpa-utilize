7db5d8c600e6b306382f19e5d36f9560a13daac1
beomjun <bzzun0609@github.com>
2024-01-14 19:37:14 +0900

    feat: MemberRepository 생성

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor        //final 설정된 필드들을 포함하는 생성자 자동 생성
+public class MemberRepository {
+
+//    @PersistenceContext     //영속성 컨텍스트 관리를 위한 Entity Manager 주입
+//    private EntityManager em;
+
+//    Required 사용으로 @Autowired 생략 가능
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        //List를 반환해야 할 경우, JPQL 사용.
+        //SQL은 테이블을 대상으로 하는 쿼리. JPQL은 엔티티 객체를 대상으로 함
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
