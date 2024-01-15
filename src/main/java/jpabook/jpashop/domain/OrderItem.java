74f2a758cbacf32d4f90c22980991f4662478306
beomjun <bzzun0609@github.com>
2024-01-15 13:42:50 +0900

    feat: OrderItem 엔티티 비즈니스 로직
    createOrderItem, cancel, getTotalPrice
    
    기본생성자 접근제어 지정

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -10,12 +10,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+//주문 단에서 orderItem을 생성할 때 new OrderItem 후 각 필드를 set 메서드로 채워넣는것을 애초에 방지하기 위해 protected 접근제어자 지정.
+@NoArgsConstructor(access = AccessLevel.PROTECTED)      //protected OrderItem() {}와 동일
 public class OrderItem {
 
     @Id
@@ -34,4 +38,26 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+
+
+    //=생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
