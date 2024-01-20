2a8771ee69e8e3727e93f867298e36e64639a99e
beomjun <bzzun0609@github.com>
2024-01-20 15:42:49 +0900

    feat: JPA로 Dto 직접 조회 + 응답 스펙 맞추기
    groupingBy 를 통해 OrderQueryDto 생성 후 OrderId에 따른 OrderItemQueryDto 생성하여 Map으로 엮은 뒤 toList

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,5 +1,9 @@
 package jpabook.jpashop.api;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,6 +13,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -81,6 +87,24 @@ public class OrderApiController {
         return orderQueryRepository.findAllByDto_optimization();
     }
 
+    //페이징 처리 불가
+//    @GetMapping("api/v6/orders")
+//    public List<OrderFlatDto> ordersV6() {
+//        return orderQueryRepository.findAllByDto_flat();
+//    }
+
+    //응답 스펙을 OrderQueryDto 로 맞추고 싶을 경우
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> orderV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o->new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o->new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(),o.getCount()), toList())
+                        )).entrySet().stream()
+                .map(e->new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue()))
+                        .collect(toList());
+    }
 
     @Getter
     static class OrderDto {
